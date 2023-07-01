af218f87fd3e006aba92047d45d18afa5a7cb0ab
TheEinshine <theeinshine3301@gmail.com>
2023-07-01 22:36:43 +0530

    fixed icons

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import "../assets/css/Menu.css";
-import "../images/vaccination(1).png";
 
 const Menu = () => {
   return (
     <nav className="menu">
-      <input
-        id="menu-toggler"
-        defaultChecked
-        className="menu-toggler"
-        type="checkbox"
-      />
+      <input id="menu-toggler" defaultChecked className="menu-toggler" type="checkbox" />
       <label htmlFor="menu-toggler"></label>
       <ul>
         <li className="menu-item">
