34f2f013c60018a5b4a505b57e76a84c4f61877a
TheEinshine <theeinshine3301@gmail.com>
2023-07-01 23:02:29 +0530

    checking

--- src/components/BMI.jsx
+++ src/components/BMI.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const BMI = () => {
-  return (
-    <div>BMI</div>
-  )
-}
+  const [height, setHeight] = useState(0.6);
+  const [weight, setWeight] = useState(20);
 
-export default BMI
+  const calculateBMI = () => {
+    const bmi = weight / ((height / 100) * (height / 100));
+    return bmi.toFixed(2);
+  };
+
+  return (
+    <>
+      <div>
+        <label>Height (in centimeters):</label>
+        <input
+          type="number"
+          step="0.01"
+          value={height}
+          onChange={(e) => setHeight(parseFloat(e.target.value))}
+        />
+      </div>
+      <div>
+        <label>Weight (in kilograms):</label>
+        <input
+          type="number"
+          step="0.01"
+          value={weight}
+          onChange={(e) => setWeight(parseFloat(e.target.value))}
+        />
+      </div>
+      <div>
+        <button onClick={calculateBMI}>Calculate BMI</button>
+      </div>
+      <div>
+        <p>Your BMI: {calculateBMI()}</p>
+      </div>
+    </>
+  );
+};
+
+export default BMI;
